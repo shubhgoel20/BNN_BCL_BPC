810c909ba2e203cdcc00cf895005b0372e57342a
Utkarsh Gupta <utkarshg99@gmail.com>
2025-01-14 11:23:38 +0100

    add skip UCB plots

--- scripts/generate_plots.py
+++ scripts/generate_plots.py
@@ -2,11 +2,14 @@ import sys
 import matplotlib.pyplot as plt
 from reader import get_info
 
-def generate_plot(experiment_name):
+def generate_plot(experiment_name, skip_approaches):
 
     data_dict, approach_data = get_info(experiment_name)
 
     num_tasks = data_dict['N']
+
+    skip_approaches = skip_approaches.split(",")
+    data_dict["M"] -= len(skip_approaches)
 
     plots_per_row = 5
     rows = num_tasks // plots_per_row if num_tasks > plots_per_row else 1
@@ -20,6 +23,8 @@ def generate_plot(experiment_name):
         row, col = divmod(task_idx, plots_per_row)
         ax = axes[row, col]
         for exp_idx, (key, experiment_data) in enumerate(approach_data.items()):
+            if key in skip_approaches:
+                continue
             times = []
             accuracies = []
             for time_step, accuracy in enumerate(experiment_data[:, task_idx]):
@@ -43,11 +48,14 @@ def generate_plot(experiment_name):
     fig.legend(handles, labels, loc='upper center', ncol=data_dict['M'])
 
     plt.tight_layout(rect=[0, 0, 1, 0.95])
-    plt.savefig(f'plots/{experiment_name}.jpg', format='jpg')
+    plot_name = f'plots/{experiment_name}.jpg'
+    if len(skip_approaches):
+        plot_name = f'plots/{experiment_name}_SKIP_{"_".join(skip_approaches)}.jpg'
+    plt.savefig(plot_name, format='jpg')
     plt.close()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python generate_plots.py <experiment_name>")
         sys.exit(1)
-    generate_plot(sys.argv[1])
+    generate_plot(sys.argv[1], sys.argv[2] if len(sys.argv) == 3 else "")
